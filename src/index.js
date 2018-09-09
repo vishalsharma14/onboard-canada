f4fcee576d6680a556cc3edeec0175c48ef50a83
vishalsharma14 <vishal.sharma@hashedin.com>
2018-09-09 17:55:08 -0400

    Removed lint errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import express from "express";
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ "name": "Onboarding Canada" });
+  res.send({ name: "Onboarding Canada" });
 });
 
 app.listen(process.env.PORT || 3000, (data, err) => {
